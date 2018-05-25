1b60fb31852d8f1172eed5e54e623b75595dce84
szemenyeim <szemenyeim@gmail.com>
2018-05-25 09:52:45 +0200

    Database converter

--- DBConvert.py
+++ DBConvert.py
@@ -1,13 +1,11 @@
-import torch
 import os.path as osp
-from torch.utils import data
 import glob
 import cv2
 import numpy as np
-import random
-import matplotlib.pyplot as plt
 import re
 import os
+from sklearn.cluster import KMeans
+import pickle
 
 def tryint(s):
     try:
@@ -21,9 +19,6 @@ def alphanum_key(s):
     """
     return [ tryint(c) for c in re.split('([0-9]+)', s) ]
 
-def default_loader(path):
-    return Image.open(path)
-
 def get_immediate_subdirectories(a_dir):
     return [name for name in os.listdir(a_dir)
             if os.path.isdir(os.path.join(a_dir, name))]
@@ -31,17 +26,19 @@ def get_immediate_subdirectories(a_dir):
 def convert(root, split="train"):
 
     labels = []
+    images = []
     preds = []
 
     data_dir = osp.join(root, split)
     lab_dir = osp.join(data_dir,"labels")
+    img_dir = osp.join(data_dir,"images")
 
     for file in sorted(glob.glob1(lab_dir, "*.png"),key=alphanum_key):
         labels.append(file)
+    for file in sorted(glob.glob1(img_dir, "*.png"),key=alphanum_key):
+        images.append(file)
 
-    cntr = 0
-
-    for file in labels:
+    for file, img in zip(labels,images):
         path = osp.join(lab_dir,file)
         label = cv2.imread(path,0)
 
@@ -59,16 +56,12 @@ def convert(root, split="train"):
                 candidates.append( cv2.boundingRect(candidate))
                 areas.append(  area )
 
-        if len(areas) > 0:
-            ball = []
-            maxArea = max(areas)
-            for cand,area in zip(candidates,areas):
-                if area >= maxArea:
-                    ball.append(cand)
-
-            pred.append([1,ball[0]])
-        else:
-            pred.append([0,(0,0,0,0)])
+        ball = []
+        maxArea = max(areas) if len(areas) > 0 else 0
+        for area, cand in sorted(zip(areas, candidates)):
+            if area >= maxArea*0.05 and len(ball) < 6:
+                ball.append(cand)
+                pred.append([1, np.asarray(cand)])
 
         # Detect robots
         robots = np.array(label == 2, dtype=np.uint8)
@@ -85,12 +78,9 @@ def convert(root, split="train"):
         robot = []
         maxArea = max(areas) if len(areas) > 0 else 0
         for area, cand in sorted(zip(areas, candidates)):
-            if area >= maxArea*0.2 and len(robot) < 5:
+            if area >= maxArea*0.05 and len(robot) < 5:
                 robot.append(cand)
-                pred.append([1, cand])
-
-        for i in range(len(robot),5):
-            pred.append([0, (0, 0, 0, 0)])
+                pred.append([2, np.asarray(cand)])
 
         # Detect goals
         goals = np.array(label == 3, dtype=np.uint8)
@@ -107,40 +97,51 @@ def convert(root, split="train"):
         goal = []
         maxArea = max(areas) if len(areas) > 0 else 0
         for area, cand in sorted(zip(areas, candidates)):
-            if area >= maxArea*0.25 and len(goal) < 2:
+            if area >= maxArea*0.2 and len(goal) < 2:
                 goal.append(cand)
-                pred.append([1, cand])
-
-        cntr += len(goal)
-        for i in range(len(goal),2):
-            pred.append([0, (0, 0, 0, 0)])
-
-        lines = np.array(label == 4, dtype=np.uint8)
-        img = np.zeros(lines.shape, dtype=np.uint8)
-        edges = np.zeros(lines.shape, dtype=np.uint8)
-        _,cont,_ = cv2.findContours(lines, mode=cv2.RETR_LIST , method=cv2.CHAIN_APPROX_SIMPLE)
-        cv2.drawContours(edges,cont,-1,255,thickness=1)
-        hough = cv2.HoughLinesP(edges,2,np.pi/180,50,20,20)
-        if hough is not None:
-            for h in hough:
-                for x1, y1, x2, y2 in h:
-                    cv2.line(img, (x1, y1), (x2, y2), (255), 2)
-            cv2.imshow("line",edges)
-            cv2.imshow("hough",img)
-            cv2.waitKey(0)
-        else:
-            print('None')
+                pred.append([3, np.asarray(cand)])
 
 
-
-
-        #print(pred)
+        '''orig_file = osp.join(img_dir,img)
+        img = cv2.imread(orig_file)
+        for i, elem in enumerate(pred):
+            if i >= 1 and elem[0] > 0:
+                color = (0,0,255) if elem[0] == 1 else ((0,255,0) if elem[0] == 2 else (255,0,0))
+                rect = list(elem[1])
+                pt1 = rect[0:2]
+                pt2 = (rect[0] + rect[2], rect[1] + rect[3])
+                img = cv2.rectangle(img,tuple(pt1),tuple(pt2),color,3)
+        cv2.imshow('img',img)
+        cv2.waitKey(0)'''
 
 
         preds.append(pred)
 
-    print(cntr)
-    print(len(labels))
+    # do clustering
+    ballRects = np.empty((0,4))
+    goalRects = np.empty((0,4))
+    robotRects = np.empty((0,4))
+
+    for pred in preds:
+        for i, elem in enumerate(pred):
+            if i > 0 and elem[0] > 0:
+                if elem[0] == 1:
+                    ballRects = np.append(ballRects,[elem[1]],axis=0)
+                elif elem[0] == 2:
+                    robotRects =np.append(robotRects,[elem[1]],axis=0)
+                else:
+                    goalRects = np.append(goalRects,[elem[1]],axis=0)
+
+    bMean = np.mean(ballRects,axis=0)
+    kmr = KMeans(5).fit(robotRects)
+    kmg = KMeans(2).fit(goalRects)
+
+    np.save(osp.join(data_dir,'bMean.npy'),bMean)
+    np.save(osp.join(data_dir,'rMean.npy'),kmr)
+    np.save(osp.join(data_dir,'gMean.npy'),kmg)
+
+    with open(osp.join(data_dir,'preds.pickle'), 'wb') as f:
+        pickle.dump(preds, f)
 
 if __name__ == '__main__':
-    convert(root='./data/FinetuneHorizon/')
+    convert(root='./data/')
